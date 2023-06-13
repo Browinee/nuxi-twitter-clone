670a716705e46b1d156e1af1a0e3a7c8f0c4b9f1
Browinee <nihilitypeo@gmail.com>
2023-06-13 21:18:35 +0800

    feat: add left and logo components

--- app.vue
+++ app.vue
@@ -1,12 +1,14 @@
 <template>
   <div :class="{ dark: darkMode }">
-    <div class="min-h-full">
-      <div
-        class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
-      >
-        <div class="bg-red-500 md:block xs-col-span-1 xl:col-span-2">
-          <div class="sticky top-0">
-            <SidebarLeft />
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div
+          class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
+        >
+          <div class="md:block xs-col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <SidebarLeft />
+            </div>
           </div>
         </div>
       </div>
@@ -14,5 +16,5 @@
   </div>
 </template>
 <script setup>
-const darkMode = ref(true);
+const darkMode = ref(false);
 </script>
