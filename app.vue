0b2100b56615af321af45ffea4efca60364e1a60
Browinee <nihilitypeo@gmail.com>
2023-06-18 17:23:12 +0800

    feat: add right side bar

--- app.vue
+++ app.vue
@@ -5,10 +5,18 @@
         <div
           class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
         >
-          <div class="md:block xs-col-span-1 xl:col-span-2">
+          <div
+            class="hidden md:block xs:col-span-1 xl:col-span-2 bg-yellow-200"
+          >
             <div class="sticky top-0">
               <SidebarLeft />
             </div>
+          </div>
+          <main class="col-span-12 md:col-span-8 xl:col-span-6 bg-red-500">
+            <h1>123123</h1>
+          </main>
+          <div class="hidden md:block md:col-span-3 xl:col-span-4 bg-blue-500">
+            <div class="sticky top-0"><SidebarRight /></div>
           </div>
         </div>
       </div>
---
+++ components/Sidebar/Right/index.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="flex flex-col">
+    <SidebarRightPreviewCard title="What's happening">
+      <SidebarRightPreviewCardItem v-for="whatsHappening in events">
+        <div>
+          <h2 class="font-bold text-gray-800 text-md dark:text-white">
+            {{ whatsHappening.title }}
+          </h2>
+
+          <p class="text-xs text-gray-400">
+            {{ whatsHappening.count }}
+          </p>
+        </div>
+      </SidebarRightPreviewCardItem>
+    </SidebarRightPreviewCard>
+    <SidebarRightPreviewCard title="Who to follow">
+      <SidebarRightPreviewCardItem v-for="person in whoToFollow">
+        <div class="flex flex-row items-center justify-between p-2">
+          <div class="flex flex-row">
+            <img
+              class="w-10 h-10 rounded-full"
+              :src="person.image"
+              :alt="person.name"
+            />
+
+            <div class="flex flex-col ml-2">
+              <h1 class="text-sm font-bold text-gray-900 dark:text-white">
+                {{ person.name }}
+              </h1>
+              <p class="text-xs text-gray-400">{{ person.handle }}</p>
+            </div>
+          </div>
+          <div class="">
+            <button
+              class="px-4 py-2 text-xs font-bold text-white bg-black rounded-full dark:text-black dark:bg-white"
+            >
+              Follow
+            </button>
+          </div>
+        </div>
+      </SidebarRightPreviewCardItem>
+    </SidebarRightPreviewCard>
+  </div>
+</template>
+<script setup>
+const { defaultTransition } = useTailwindConfig();
+const props = defineProps({
+  active: {
+    type: Boolean,
+    default: false,
+  },
+});
+const textClass = computed(() => (props.active ? "font-semibold" : "normal"));
+const events = [
+  {
+    title: "SpaceX",
+    count: "18.8k Tweets",
+  },
+  {
+    title: "#CodingIsFun",
+    count: "8.8k Tweets",
+  },
+  {
+    title: "#artforall",
+    count: "1.8k Tweets",
+  },
+];
+
+const whoToFollow = [
+  {
+    name: "Joe Biden",
+    handle: "@JoeBiden",
+    image: "https://picsum.photos/200/200",
+  },
+  {
+    name: "Joe Biden",
+    handle: "@JoeBiden",
+    image: "https://picsum.photos/200/200",
+  },
+  {
+    name: "Joe Biden",
+    handle: "@JoeBiden",
+    image: "https://picsum.photos/200/200",
+  },
+];
+</script>
